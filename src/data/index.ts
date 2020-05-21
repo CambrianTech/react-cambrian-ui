4588260a2d02fbb78adbaf8a7060b9026a551f52
Joel Teply <joel@cambriantech.com>
2020-05-20 21:34:12 -0500

    panel cleanup

--- src/data/index.ts
+++ src/data/index.ts
@@ -8,3 +8,5 @@ export * from "./ProductColor"
 
 //interfaces and abstract classes
 export * from "./SwatchItem"
+
+console.log(`@SDK_NAME@ version @SDK_VERSION@ by cambrian.io`)
--- src/visualizer/VisualizerTools.tsx
+++ src/visualizer/VisualizerTools.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import {Fab} from "@material/react-fab";
 import '@material/react-fab/dist/fab.css';
-import {useState, useCallback} from "react";
-import {CBARAsset, CBARSurface} from "react-home-ar";
+import {useState, useCallback, useEffect} from "react";
+import {CBARContext} from "react-home-ar";
 import MaterialIcon from "@material/react-material-icon";
 import {RotateTool} from "./RotateTool";
 import classes from "./VisualizerTools.scss";
@@ -17,27 +17,25 @@ export enum VisualizerToolMode {
 type VisualizerToolsProperties = {
     visible:boolean
 
+    context:CBARContext|undefined
+
     mode:VisualizerToolMode
     historySize:number
 
     onChangeImage:()=>void
     onShowHideButtons: (show: boolean) => void
 
-    selectedSurface?:CBARSurface
-    selectedAsset?:CBARAsset
-
     initialRotation:number
     onRotationChanged: (radians: number) => void
     onRotationFinished: (commit: boolean, radians:number) => void
 
-    editAreaEnabled?:boolean
-
-    onModeChanged: (mode:VisualizerToolMode) => void
+    changeMode: (mode:VisualizerToolMode) => void
 }
 
 export function VisualizerTools(props: VisualizerToolsProperties) {
 
-    const [isOpen, setIsOpen] = useState<boolean>()
+
+    const currentScene = props.context ? props.context.getScene() : undefined
 
     const onChangeImage = useCallback((e:any) => {
         props.onChangeImage()
@@ -46,33 +44,30 @@ export function VisualizerTools(props: VisualizerToolsProperties) {
     const onShowHideButtons = props.onShowHideButtons
     const onRotationFinished = props.onRotationFinished
 
-    const toggleTools = useCallback(() => {
-        const open = !isOpen
-        setIsOpen(open)
-    } , [isOpen]);
-
     const rotateFinished = useCallback((finished: boolean, radians:number) => {
         onShowHideButtons(true)
         onRotationFinished(finished, radians)
-        props.onModeChanged(VisualizerToolMode.None)
+        props.changeMode(VisualizerToolMode.None)
     } , [onRotationFinished, onShowHideButtons, props]);
 
     const rotateButtonClicked = useCallback(() => {
-        toggleTools()
-        props.onModeChanged(VisualizerToolMode.None)
-    } , [props, toggleTools]);
+        props.changeMode(VisualizerToolMode.Rotate)
+    } , [props]);
 
     return (
-        <div className={classes.visualizerTools}>
+        <div>
+            {props.visible && props.context && (
+                <div className={classes.visualizerTools}>
+                    <Fab className={classes.toolButton} onClick={onChangeImage} icon={<MaterialIcon icon='add_a_photo' />} />
+                    {currentScene && <Fab className={classes.toolButton} onClick={rotateButtonClicked} icon={<MaterialIcon icon='rotate_right' />} />}
+                    {currentScene && currentScene.isEditable && <Fab className={classes.toolButton} icon={<MaterialIcon icon='edit' />} />}
 
-            {props.visible && <Fab className={classes.toolButton} onClick={onChangeImage} icon={<MaterialIcon icon='add_a_photo' />} />}
-            {props.visible && <Fab className={classes.toolButton} onClick={rotateButtonClicked} icon={<MaterialIcon icon='rotate_right' />} />}
-            {props.visible && props.editAreaEnabled && <Fab className={classes.toolButton} icon={<MaterialIcon icon='edit' />} />}
-
-            <RotateTool visible={props.mode === VisualizerToolMode.Rotate}
-                        onRotationFinished={rotateFinished}
-                        onRotationChanged={props.onRotationChanged}
-                        rotation={props.initialRotation} />
+                    <RotateTool visible={props.mode === VisualizerToolMode.Rotate}
+                                onRotationFinished={rotateFinished}
+                                onRotationChanged={props.onRotationChanged}
+                                rotation={props.initialRotation} />
+                </div>
+            )}
         </div>
     )
 }
