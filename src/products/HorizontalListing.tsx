4ab1ecdc987b6b6c0ed7b59499f9b2de79308e66
Joel Teply <joel@cambriantech.com>
2020-05-23 20:28:28 -0500

    filtering

--- src/products/HorizontalListing.tsx
+++ src/products/HorizontalListing.tsx
@@ -24,7 +24,8 @@ export class HorizontalListing extends SwatchListing<HorizontalListingProps> {
         return (
             <div key={swatch.key} onClick={()=>this.props.onClick(swatch)} className={className}>
                 <div className={appendClassName("horizontal-swatch-listing-details", classes.swatchListingDetails)}>
-                    <Thumbnail className={appendClassName("horizontal-swatch-listing-image", classes.swatchListingImage)} swatch={swatch} resolveThumbnailPath={this.props.resolveThumbnailPath} />
+                    <Thumbnail className={appendClassName("horizontal-swatch-listing-image", classes.swatchListingImage)}
+                               swatch={swatch} resolveThumbnailPath={this.props.resolveThumbnailPath} />
                     <div className={appendClassName("horizontal-swatch-listing-info", classes.swatchListingInfo)}>
                         {swatch.displayName}
                     </div>
--- src/products/SwatchListing.tsx
+++ src/products/SwatchListing.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import {CBARAssetType, SwatchItem} from "react-home-ar";
+import {CBARAssetType, ProductBase, SwatchItem} from "react-home-ar";
 import {appendClassName} from "../internal/Utils";
 import {ReactNode} from "react";
 
-export type SwatchListingProps = {
+export interface SwatchListingProps {
     className?:string
     swatches?:SwatchItem[]
     selectedSwatch?:SwatchItem
@@ -12,8 +12,8 @@ export type SwatchListingProps = {
     filters?:SwatchListingFilter[]
 }
 
-export type SwatchListingFilter = {
-    assetType:CBARAssetType
+export interface SwatchListingFilter {
+    assetType?:CBARAssetType
 }
 
 export abstract class SwatchListing<T extends SwatchListingProps> extends React.Component<T> {
@@ -22,6 +22,49 @@ export abstract class SwatchListing<T extends SwatchListingProps> extends React.
     }
 
     protected abstract renderSwatch(swatch:SwatchItem): ReactNode;
+
+    protected matchesFilter(swatch:SwatchItem, filter:SwatchListingFilter) : boolean {
+        const product = swatch as ProductBase
+        if (filter.assetType && product) {
+            return product.assetTypes.indexOf(filter.assetType) >= 0
+        }
+        return true
+    }
+
+    protected get swatches() : SwatchItem[] {
+
+        if (this.props.swatches) {
+            let swatches:SwatchItem[] = this.props.swatches as SwatchItem[]
+
+            if (this.props.filters) {
+                this.props.filters.forEach(filter => {
+                    swatches = swatches.filter((swatch)=>{
+                        return this.matchesFilter(swatch, filter)
+                    })
+                })
+            }
+
+            return swatches
+        }
+
+        return []
+    }
+
+    private filtersChanged(nextProps: Readonly<T>) {
+        if (nextProps.filters !== this.props.filters) return true
+        if (nextProps.filters && this.props.filters) {
+            if (nextProps.filters.length !== this.props.filters.length) {
+                return true
+            }
+            return JSON.stringify(nextProps.filters) !== JSON.stringify(this.props.filters)
+        }
+        return false
+    }
+
+    // shouldComponentUpdate(nextProps: Readonly<T>, nextState: Readonly<{}>, nextContext: any): boolean {
+    //     const selectedSwatchChanged = nextProps.selectedSwatch !== this.props.selectedSwatch
+    //     return selectedSwatchChanged || this.filtersChanged(nextProps)
+    // }
 
     render() {
         if (!this.props.swatches) {
@@ -34,7 +77,7 @@ export abstract class SwatchListing<T extends SwatchListingProps> extends React.
         return (
             <div className={className}>
                 <div className={appendClassName(`${this.listingName}-content`, this.scss.swatchListingContent)}>
-                    {this.props.swatches.map((swatch) => {
+                    {this.swatches.map((swatch) => {
                         return this.renderSwatch(swatch)
                     })}
                 </div>
