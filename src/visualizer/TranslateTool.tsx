4bce98453cf7ef05e6fbea08c36a44854e514872
Joel Teply <joel@cambriantech.com>
2020-05-27 13:38:16 -0500

    translation tool,

---
+++ src/visualizer/TranslateTool.tsx
@@ -0,0 +1,109 @@
+import * as React from "react";
+import {Fab} from "@material/react-fab";
+import '@material/react-fab/dist/fab.css';
+import {useRef} from "react";
+
+import classes from "./TranslateTool.scss";
+import MaterialIcon from "@material/react-material-icon";
+
+import {appendClassName} from "../internal/Utils"
+
+type TranslateToolProps = {
+    visible: boolean
+    className?:string
+    xPos: number
+    yPos: number
+    onTranslationFinished: (commit: boolean, xPos: number, yPos: number) => void
+    onTranslationChanged: (xPos: number, yPos: number) => void
+}
+
+type TranslateToolCachedProps = TranslateToolProps & {
+    onTranslationXChanged:(xPos: number) => void
+    onTranslationYChanged:(yPos: number) => void
+    onTranslationFinishedInternal: (commit: boolean) => void
+}
+
+export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
+    (cProps) => {
+        if (cProps.visible) {
+            let className = appendClassName("translate-tool", classes.translateTool)
+            className = appendClassName(className, cProps.className)
+
+            return (
+                <div className={className}>
+                    <div>
+                        <div className={classes.translateToolSlider}>
+                            <div className={classes.translateToolSliderLabels}>
+                                <div>-10</div>
+                                <div>|&nbsp;</div>
+                                <div>-5°</div>
+                                <div>|&nbsp;</div>
+                                <div>0</div>
+                                <div>|&nbsp;</div>
+                                <div>5</div>
+                                <div>|&nbsp;</div>
+                                <div>10</div>
+                            </div>
+                            <div className={classes.translateToolSliderBar}>
+                                <input type="range" min={-10} max={10} step={0.01} defaultValue={cProps.xPos + ""}
+                                       onChange={e => cProps.onTranslationXChanged(Number(e.target.value))} list="range-values" />
+                            </div>
+
+                            <div className={classes.translateToolSliderBar}>
+                                <input type="range" min={-10} max={10} step={0.01} defaultValue={cProps.yPos + ""}
+                                       onChange={e => cProps.onTranslationYChanged(Number(e.target.value))} list="range-values" />
+                            </div>
+
+                            <datalist id="range-values">
+                                <option value="-10" />
+                                <option value="-7.5" />
+                                <option value="-5" />
+                                <option value="-2.5" />
+                                <option value="0" />
+                                <option value="2.5" />
+                                <option value="5.0" />
+                                <option value="7.5" />
+                                <option value="10" />
+                            </datalist>
+                        </div>
+                        <div className={classes.translateToolSliderFooter}>
+                            <Fab className={classes.translateToolSliderButton} icon={<MaterialIcon icon='close' />} style={{backgroundColor:"#555"}} onClick={() => cProps.onTranslationFinishedInternal(false)}  />
+                            <Fab className={classes.translateToolSliderButton} icon={<MaterialIcon icon='check' />} onClick={() => cProps.onTranslationFinishedInternal(true)} />
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return (<aside />);
+    },
+    (prevProps, nextProps) => {
+        return prevProps.visible === nextProps.visible;
+    }
+);
+
+export function TranslateTool(props: TranslateToolProps) {
+    const translationControl_xPos = useRef(0)
+    const translationControl_yPos = useRef(0)
+
+    function translationXChanged(xPos: number) {
+        translationControl_xPos.current = xPos
+        props.onTranslationChanged(xPos, translationControl_yPos.current)
+    }
+
+    function translationYChanged(yPos: number) {
+        translationControl_yPos.current = yPos
+        props.onTranslationChanged(translationControl_xPos.current, yPos)
+    }
+
+    function translationFinished(commit: boolean) {
+        console.log(`translationFinished: ${commit}, ${translationControl_xPos.current}, ${translationControl_yPos.current}`)
+        props.onTranslationFinished(commit, translationControl_xPos.current, translationControl_yPos.current)
+    }
+
+    return (
+        <TranslateToolCached onTranslationXChanged={translationXChanged}
+                             onTranslationYChanged={translationYChanged}
+                             onTranslationFinishedInternal={translationFinished}
+                             {...props} />
+    )
+}
--- src/visualizer/VisualizerTools.tsx
+++ src/visualizer/VisualizerTools.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import {Fab} from "@material/react-fab";
 import '@material/react-fab/dist/fab.css';
 import {useCallback} from "react";
-import {CBARAsset, CBARContext, CBARSurface, CBARTangibleAsset} from "react-home-ar";
+import {CBARContext, CBARSurface, CBARTangibleAsset} from "react-home-ar";
 import MaterialIcon from "@material/react-material-icon";
 import {RotateTool} from "./RotateTool";
 import classes from "./VisualizerTools.scss";
 import {appendClassName} from "../internal/Utils"
+import {TranslateTool} from "./TranslateTool";
 
 export enum VisualizerToolMode {
     None,
@@ -34,6 +35,11 @@ type VisualizerToolsProperties = {
     onRotationChanged: (radians: number) => void
     onRotationFinished: (commit: boolean, radians:number) => void
 
+    initialXPos:number
+    initialYPos:number
+    onTranslationChanged: (xPos: number, yPos: number) => void
+    onTranslationFinished: (commit: boolean, xPos: number, yPos: number) => void
+
     changeMode: (mode:VisualizerToolMode) => void
 }
 
@@ -46,6 +52,7 @@ export function VisualizerTools(props: VisualizerToolsProperties) {
     }, [props])
 
     const onShowHideButtons = props.onShowHideButtons
+
     const onRotationFinished = props.onRotationFinished
 
     const rotateFinished = useCallback((finished: boolean, radians:number) => {
@@ -57,6 +64,13 @@ export function VisualizerTools(props: VisualizerToolsProperties) {
     const rotateButtonClicked = useCallback(() => {
         props.changeMode(VisualizerToolMode.Rotate)
     } , [props]);
+
+    const onTranslationFinished = props.onTranslationFinished
+    const translateFinished = useCallback((finished: boolean, xPos:number, yPos:number) => {
+        onShowHideButtons(true)
+        onTranslationFinished(finished, xPos, yPos)
+        props.changeMode(VisualizerToolMode.None)
+    } , [onTranslationFinished, onShowHideButtons, props]);
 
     const translateButtonClicked = useCallback(() => {
         props.changeMode(VisualizerToolMode.Translate)
@@ -78,6 +92,11 @@ export function VisualizerTools(props: VisualizerToolsProperties) {
                                 onRotationFinished={rotateFinished}
                                 onRotationChanged={props.onRotationChanged}
                                 rotation={props.initialRotation} />
+
+                    <TranslateTool visible={props.mode === VisualizerToolMode.Translate}
+                                   onTranslationFinished={translateFinished}
+                                   onTranslationChanged={props.onTranslationChanged}
+                                   xPos={props.initialXPos} yPos={props.initialYPos} />
                 </div>
             )}
         </div>
