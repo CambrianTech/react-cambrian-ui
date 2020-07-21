cabffa564eac9c5e53f84f48860556f816e56aca
Joel Teply <joel@cambriantech.com>
2020-07-20 19:27:56 -0500

    controls working

--- src/visualizer/TranslateTool.tsx
+++ src/visualizer/TranslateTool.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Fab} from "@material/react-fab";
 import '@material/react-fab/dist/fab.css';
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef} from "react";
 
 import classes from "./TranslateTool.scss";
 import MaterialIcon from "@material/react-material-icon";
@@ -26,21 +26,24 @@ type TranslateToolCachedProps = TranslateToolProps & {
     onTranslationXChanged:(xPos: number) => void
     onTranslationYChanged:(yPos: number) => void
     onTranslationFinishedInternal: (commit: boolean) => void
+    onMouseDown:()=>void
+    onMouseUp:()=>void
 }
 
 export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
     (cProps) => {
+
         if (cProps.visible) {
 
-            let className = appendClassName("translate-tool", classes.translateTool)
-            className = appendClassName(className, cProps.className)
+            let className = appendClassName("translate-tool", classes.translateTool);
+            className = appendClassName(className, cProps.className);
 
-            const min = cProps.min !== undefined ? cProps.min : -5
-            const max = cProps.max !== undefined ? cProps.max : 5
-            const range = max - min
-            const middle = min + range / 2
-            const minMiddle = min + range / 4
-            const maxMiddle = max - range / 4
+            const min = cProps.min !== undefined ? cProps.min : -5;
+            const max = cProps.max !== undefined ? cProps.max : 5;
+            const range = max - min;
+            const middle = min + range / 2;
+            const minMiddle = min + range / 4;
+            const maxMiddle = max - range / 4;
 
             return (
                 <div className={className}>
@@ -58,12 +61,14 @@ export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
                                 <div>{max}</div>
                             </div>
                             <div className={classes.translateToolSliderBar}>
-                                <input type="range" min={min} max={max} step={0.01} defaultValue={cProps.xPos + ""}
+                                <input id={"translate-x"} type="range" min={min} max={max} step={0.01} defaultValue={cProps.xPos + ""}
+                                       onMouseDown={()=>cProps.onMouseDown()} onMouseUp={()=>cProps.onMouseUp()}
                                        onChange={e => cProps.onTranslationXChanged(Number(e.target.value))} list="range-values" />
                             </div>
 
                             <div className={classes.translateToolSliderBar}>
-                                <input type="range" min={min} max={max} step={0.01} defaultValue={cProps.yPos + ""}
+                                <input id={"translate-y"} type="range" min={min} max={max} step={0.01} defaultValue={cProps.yPos + ""}
+                                       onMouseDown={()=>cProps.onMouseDown()} onMouseUp={()=>cProps.onMouseUp()}
                                        onChange={e => cProps.onTranslationYChanged(Number(e.target.value))} list="range-values" />
                             </div>
 
@@ -91,35 +96,58 @@ export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
 );
 
 export function TranslateTool(props: TranslateToolProps) {
-    const translationControl_xPos = useRef(0)
-    const translationControl_yPos = useRef(0)
 
-    function translationXChanged(xPos: number) {
-        translationControl_xPos.current = xPos
-        props.onTranslationChanged(xPos, translationControl_yPos.current)
-    }
+    const translationControl_xPos = useRef(0);
+    const translationControl_yPos = useRef(0);
+    const onTranslationChanged = props.onTranslationChanged;
+    const isActive = useRef(false);
 
-    function translationYChanged(yPos: number) {
-        translationControl_yPos.current = yPos
-        props.onTranslationChanged(translationControl_xPos.current, yPos)
-    }
+    const sendTranslationChanged = useCallback(() => {
+        if (!isActive.current) return;
+        onTranslationChanged(translationControl_xPos.current, translationControl_yPos.current);
+    }, [translationControl_xPos, translationControl_yPos, onTranslationChanged]);
 
-    function translationFinished(commit: boolean) {
+    const translationXChanged = useCallback((xPos: number) => {
+        translationControl_xPos.current = xPos;
+        sendTranslationChanged()
+    }, [translationControl_xPos, sendTranslationChanged]);
+
+    const translationYChanged = useCallback((yPos: number) => {
+        translationControl_yPos.current = yPos;
+        sendTranslationChanged()
+    }, [translationControl_yPos, sendTranslationChanged]);
+
+    const translationFinished = useCallback((commit: boolean) => {
         props.onTranslationFinished(commit, translationControl_xPos.current, translationControl_yPos.current)
-    }
+    }, []);
+
+    const [visible, xPos, yPos] = [props.visible, props.xPos, props.yPos]
 
     useEffect(()=>{
-        if (props.visible) {
-            translationControl_xPos.current = props.xPos
-            translationControl_yPos.current = props.yPos
+        if (visible) {
+            translationControl_xPos.current = xPos;
+            translationControl_yPos.current = yPos;
+            if (!isActive.current) {
+                const translateX = document.getElementById("translate-x") as HTMLInputElement;
+                if (translateX) {
+                    translateX.value = `${xPos}`
+                }
+                const translateY = document.getElementById("translate-y") as HTMLInputElement;
+                if (translateY) {
+                    translateY.value = `${yPos}`
+                }
+            }
         }
-    }, [props.visible])
+    }, [visible, xPos, yPos, translationControl_xPos, translationControl_yPos]);
 
     return (
         <TranslateToolCached {...props}
+                             onMouseDown={()=>isActive.current = true}
+                             onMouseUp={()=>isActive.current = false}
+
                              onTranslationXChanged={translationXChanged}
                              onTranslationYChanged={translationYChanged}
                              onTranslationFinishedInternal={translationFinished}
-                             />
+        />
     )
 }
