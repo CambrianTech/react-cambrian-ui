eaae0bc55a554f1150bdab584dc8e3fd45f541c6
Joel Teply <joel@cambriantech.com>
2020-07-25 21:12:45 -0500

    details

---
+++ src/products/ProductInfo.tsx
@@ -0,0 +1,52 @@
+import {default as React} from "react";
+import {ProductItem} from "react-home-ar";
+import classes from "./ProductInfo.scss";
+import {appendClassName} from "../internal/Utils";
+import {LazyImage} from "react-lazy-elements";
+
+type ProductInfoProps = {
+    product?:ProductItem
+    className?:string
+    resolveUrl:(name:string, url:string)=>string
+}
+
+export const ProductInfoCached = React.memo<ProductInfoProps>(
+
+    (props) => {
+        if (props.product && props.product.details) {
+
+            let className = appendClassName("product-info", classes.productInfo)
+            if (props.className) {
+                className = appendClassName(className, props.className)
+            }
+
+            const details = props.product.details
+            const specs = details.specifications
+            return (
+                <div className={className}>
+                    {details.preview && <LazyImage className={appendClassName("preview", classes.preview)}
+                                                   src={props.resolveUrl("preview", details.preview)} />}
+                    <ul className={appendClassName("specs", classes.specs)}>
+                        {Object.keys(specs).map((field) => {
+                            return <li key={field}>
+                                <span className={"field"}>{field}</span>
+                                <span className={"value"}>{specs[field]}</span>
+                            </li>
+                        })}
+                    </ul>
+                    <div>
+                        <a href={details.url}>More details...</a>
+                    </div>
+                </div>
+            );
+        }
+        return null;
+    },
+    (prevProps, nextProps) => {
+        return prevProps.product === nextProps.product;
+    }
+);
+
+export function ProductInfo(props: ProductInfoProps) {
+    return <ProductInfoCached {...props} />
+}
