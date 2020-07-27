26a3a866712b27f5128e626128fb7600be47425e
Joel Teply <joel@cambriantech.com>
2020-07-27 10:51:56 -0500

    product info

--- src/products/ProductInfo.tsx
+++ src/products/ProductInfo.tsx
@@ -15,15 +15,23 @@ export const ProductInfoCached = React.memo<ProductInfoProps>(
     (props) => {
         if (props.product && props.product.details) {
 
-            let className = appendClassName("product-info", classes.productInfo)
+            let className = appendClassName("product-info", classes.productInfo);
             if (props.className) {
-                className = appendClassName(className, props.className)
+                className = appendClassName(className, props.className);
             }
 
-            const details = props.product.details
-            const specs = details.specifications
+            const details = props.product.details;
+            const specs = details.specifications;
             return (
                 <div className={className}>
+                    <div className={appendClassName("product-info-name", classes.productName)}>
+                        {props.product.parent && <div className={appendClassName("product-info-title", classes.productTitle)}>
+                            {props.product.parent.displayName}
+                        </div>}
+                        <div className={appendClassName("product-info-sub-title", classes.productSubTitle)}>
+                            {props.product.displayName}
+                        </div>
+                    </div>
                     {details.preview && <LazyImage className={appendClassName("preview", classes.preview)}
                                                    src={props.resolveUrl("preview", details.preview)} />}
                     <ul className={appendClassName("specs", classes.specs)}>
