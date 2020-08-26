74240dae427c03abac3f00c2776cd273d06d5c31
Joel Teply <joel@cambriantech.com>
2020-08-26 13:14:11 -0500

    same changes to tool

--- src/visualizer/TranslateTool.tsx
+++ src/visualizer/TranslateTool.tsx
@@ -26,8 +26,10 @@ type TranslateToolCachedProps = TranslateToolProps & {
     onTranslationXChanged:(xPos: number) => void
     onTranslationYChanged:(yPos: number) => void
     onTranslationFinishedInternal: (commit: boolean) => void
-    onMouseDown:()=>void
-    onMouseUp:()=>void
+
+    onMouseDown:(e:any, isX:boolean)=>void
+    onMouseUp:(e:any, isX:boolean)=>void
+    onMouseMove:(e:any, isX:boolean)=>void
 }
 
 export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
@@ -49,55 +51,62 @@ export const TranslateToolCached = React.memo<TranslateToolCachedProps>(
                 <div className={className}>
                     <div className={appendClassName("translate-tool-content", classes.translateToolContent)}>
                         <div className={appendClassName("translate-tool-slider", classes.translateToolSlider)}>
-                            <div className={appendClassName("translate-tool-slider-labels", classes.translateToolSliderLabels)}>
-                                <div>{min[0]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{minMiddle[0]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{middle[0]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{maxMiddle[0]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{max[0]}</div>
-                            </div>
-                            <datalist id="range-values-0">
-                                <option value={min[0]} />
-                                <option value={minMiddle[0]} />
-                                <option value={middle[0]} />
-                                <option value={maxMiddle[0]} />
-                                <option value={max[0]} />
-                            </datalist>
 
-                            <div className={appendClassName("translate-tool-slider-bar", classes.translateToolSliderBar)}>
-                                <input id={"translate-x"} type="range" min={min[0]} max={max[0]} step={0.01} defaultValue={cProps.xPos + ""}
-                                       onMouseDown={()=>cProps.onMouseDown()} onTouchStart={()=>cProps.onMouseDown()}
-                                       onMouseUp={()=>cProps.onMouseUp()} onTouchEnd={()=>cProps.onMouseUp()}
-                                       onChange={e => cProps.onTranslationXChanged(Number(e.target.value))} list="range-values-0" />
+                            <div onMouseDown={(e)=>cProps.onMouseDown(e,true)} onTouchStart={(e)=>cProps.onMouseDown(e,true)}
+                                 onMouseMove={(e)=>cProps.onMouseMove(e,true)} onTouchMove={(e)=>cProps.onMouseMove(e,true)}
+                                 onMouseUp={(e)=>cProps.onMouseUp(e,true)} onTouchEnd={(e)=>cProps.onMouseUp(e,true)}>
+                                <div className={appendClassName("translate-tool-slider-labels", classes.translateToolSliderLabels)}>
+                                    <div>{min[0]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{minMiddle[0]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{middle[0]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{maxMiddle[0]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{max[0]}</div>
+                                </div>
+                                <datalist id="range-values-0">
+                                    <option value={min[0]} />
+                                    <option value={minMiddle[0]} />
+                                    <option value={middle[0]} />
+                                    <option value={maxMiddle[0]} />
+                                    <option value={max[0]} />
+                                </datalist>
+
+                                <div className={appendClassName("translate-tool-slider-bar", classes.translateToolSliderBar)}>
+                                    <input id={"translate-x"} type="range" min={min[0]} max={max[0]} step={0.01} defaultValue={cProps.xPos + ""}
+                                           onChange={e => cProps.onTranslationXChanged(Number(e.target.value))} list="range-values-0" />
+                                </div>
                             </div>
 
-                            <div className={appendClassName("translate-tool-slider-labels", classes.translateToolSliderLabels)}>
-                                <div>{min[1]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{minMiddle[1]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{middle[1]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{maxMiddle[1]}</div>
-                                <div>|&nbsp;</div>
-                                <div>{max[1]}</div>
-                            </div>
-                            <datalist id="range-values-1">
-                                <option value={min[1]} />
-                                <option value={minMiddle[1]} />
-                                <option value={middle[1]} />
-                                <option value={maxMiddle[1]} />
-                                <option value={max[1]} />
-                            </datalist>
+                            <div onMouseDown={(e)=>cProps.onMouseDown(e,false)} onTouchStart={(e)=>cProps.onMouseDown(e,false)}
+                                 onMouseMove={(e)=>cProps.onMouseMove(e,false)} onTouchMove={(e)=>cProps.onMouseMove(e,false)}
+                                 onMouseUp={(e)=>cProps.onMouseUp(e,false)} onTouchEnd={(e)=>cProps.onMouseUp(e,false)}>
+                                <div className={appendClassName("translate-tool-slider-labels", classes.translateToolSliderLabels)}>
+                                    <div>{min[1]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{minMiddle[1]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{middle[1]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{maxMiddle[1]}</div>
+                                    <div>|&nbsp;</div>
+                                    <div>{max[1]}</div>
+                                </div>
 
-                            <div className={appendClassName("translate-tool-slider-bar", classes.translateToolSliderBar)}>
-                                <input id={"translate-y"} type="range" min={min[1]} max={max[1]} step={0.01} defaultValue={cProps.yPos + ""}
-                                       onMouseDown={()=>cProps.onMouseDown()} onMouseUp={()=>cProps.onMouseUp()}
-                                       onChange={e => cProps.onTranslationYChanged(Number(e.target.value))} list="range-values-1" />
+                                <datalist id="range-values-1">
+                                    <option value={min[1]} />
+                                    <option value={minMiddle[1]} />
+                                    <option value={middle[1]} />
+                                    <option value={maxMiddle[1]} />
+                                    <option value={max[1]} />
+                                </datalist>
+
+                                <div className={appendClassName("translate-tool-slider-bar", classes.translateToolSliderBar)}>
+                                    <input id={"translate-y"} type="range" min={min[1]} max={max[1]} step={0.01} defaultValue={cProps.yPos + ""}
+                                           onChange={e => cProps.onTranslationYChanged(Number(e.target.value))} list="range-values-1" />
+                                </div>
                             </div>
 
                             <div className={appendClassName("translate-tool-instructions", classes.translateToolInstructions)}>
@@ -145,7 +154,46 @@ export function TranslateTool(props: TranslateToolProps) {
         props.onTranslationFinished(commit, translationControl_xPos.current, translationControl_yPos.current)
     }, []);
 
-    const [visible, xPos, yPos] = [props.visible, props.xPos, props.yPos]
+    const adjustSlider = useCallback((e:any, isX:boolean)=>{
+
+        const slider = (isX ? document.getElementById("translate-x") : document.getElementById("translate-y")) as HTMLInputElement
+
+        if (slider && e.hasOwnProperty("pageX") && e.hasOwnProperty("target")) {
+            const rect = slider.getBoundingClientRect();
+            const sliderDiameter = rect.height;
+            const x = (e.clientX - rect.left - 0.5 * sliderDiameter) / (rect.width - sliderDiameter);
+            const range = parseFloat(slider.max) - parseFloat(slider.min);
+            const value = parseFloat(slider.min) + range * x;
+
+            const currrentValue = parseFloat(slider.value);
+
+            if (Math.abs(currrentValue - value) > 0.1) {
+                slider.value = `${value}`;
+                if (isX) {
+                    translationXChanged(value);
+                } else {
+                    translationYChanged(value);
+                }
+            }
+        }
+    }, [translationXChanged, translationYChanged]);
+
+    const onMouseDown = useCallback((e:any, isX:boolean) => {
+        isActive.current = true;
+        adjustSlider(e, isX);
+    }, [isActive, adjustSlider]);
+
+    const onMouseMove = useCallback((e:any, isX:boolean) => {
+        if (isActive.current) {
+            adjustSlider(e, isX);
+        }
+    }, [adjustSlider]);
+
+    const onMouseUp = useCallback((e:any) => {
+        isActive.current = false;
+    }, [isActive, adjustSlider]);
+
+    const [visible, xPos, yPos] = [props.visible, props.xPos, props.yPos];
 
     useEffect(()=>{
         if (visible) {
@@ -166,9 +214,9 @@ export function TranslateTool(props: TranslateToolProps) {
 
     return (
         <TranslateToolCached {...props}
-                             onMouseDown={()=>isActive.current = true}
-                             onMouseUp={()=>isActive.current = false}
-
+                             onMouseDown={onMouseDown}
+                             onMouseMove={onMouseMove}
+                             onMouseUp={onMouseUp}
                              onTranslationXChanged={translationXChanged}
                              onTranslationYChanged={translationYChanged}
                              onTranslationFinishedInternal={translationFinished}
