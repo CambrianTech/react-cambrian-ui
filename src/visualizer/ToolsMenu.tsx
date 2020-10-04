860b15871539c6f54068cbf51e88b5c753de4e5d
Joel Teply <joel@cambriantech.com>
2020-10-04 18:02:53 -0500

    make a better tools menu

--- src/visualizer/ToolsMenu.tsx
+++ src/visualizer/ToolsMenu.tsx
@@ -6,15 +6,26 @@ import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
 import {makeStyles} from "@material-ui/core/styles";
 import MaterialIcon from "@material/react-material-icon";
 import {SpeedDialIcon} from "@material-ui/lab";
+import {RotateTool} from "./RotateTool";
+import {TranslateTool} from "./TranslateTool";
+import {useCallback} from "react";
 
-type ToolsMenuProperties = {
+export enum ToolMode {
+    None='',
+    Rotate='rotate',
+    Translate='translate',
+    DrawSurface='draw',
+    EraseSurface='erase'
+}
+
+export type SimpleToolsMenuProperties = {
     className?:string
     hidden?:boolean
     direction?:'up' | 'down' | 'left' | 'right'
     actions?:ToolsMenuAction[]
     icon?:React.ReactNode,
     openIcon?:React.ReactNode
-    onAction?:(action:ToolsMenuAction)=>void
+    onAction:(action:ToolsMenuAction)=>void
 }
 
 export type ToolsMenuAction = {
@@ -28,11 +39,27 @@ export const ToolsMenuDefaultActions:ToolsMenuAction[] = [
     { icon: <MaterialIcon icon='add_a_photo' />, name: 'Photo', longName:'Take Photo' },
     { icon: <MaterialIcon icon='insert_photo' />, name: 'Scene', longName:'Change Scene' },
     { icon: <MaterialIcon icon='share' />, name: 'Share', longName:'Share Project' },
-    { icon: <MaterialIcon icon='rotate_right' />, name: 'Rotate' },
-    { icon: <MaterialIcon icon='open_with' />, name: 'Move' },
+    { icon: <MaterialIcon icon='rotate_right' />, name: 'Rotate', operation:ToolMode.Rotate },
+    { icon: <MaterialIcon icon='open_with' />, name: 'Move', operation:ToolMode.Translate },
     { icon: <MaterialIcon icon='view_compact' />, name: 'Pattern' },
-    { icon: <MaterialIcon icon='edit' />, name: 'Edit Surface' },
+    { icon: <MaterialIcon icon='edit' />, name: 'Edit Surface', operation:ToolMode.DrawSurface },
 ];
+
+export type ToolsMenuProperties = SimpleToolsMenuProperties & {
+
+    initialRotation?:number
+    onRotationChanged?: (radians: number) => void
+    onRotationFinished?: (commit: boolean, radians:number) => void
+    onShowHideButtons: (show: boolean) => void
+
+    initialXPos?:number
+    initialYPos?:number
+    minTranslation?:[number, number]
+    maxTranslation?:[number, number]
+
+    onTranslationChanged?: (xPos: number, yPos: number) => void
+    onTranslationFinished?: (commit: boolean, xPos: number, yPos: number) => void
+}
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -47,7 +74,7 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-export function ToolsMenu(props: ToolsMenuProperties) {
+export function SimpleToolsMenu(props: SimpleToolsMenuProperties) {
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
     const actions = props.actions ? props.actions : ToolsMenuDefaultActions;
@@ -62,10 +89,8 @@ export function ToolsMenu(props: ToolsMenuProperties) {
     };
 
     const handleAction = (action:ToolsMenuAction) => {
-        handleClose();
-        if (props.onAction) {
-            props.onAction(action);
-        }
+        setOpen(false);
+        props.onAction(action);
     };
 
     return (
@@ -88,5 +113,73 @@ export function ToolsMenu(props: ToolsMenuProperties) {
                 />
             ))}
         </SpeedDial>
+    )
+}
+
+export function ToolsMenu(props: ToolsMenuProperties) {
+
+    const [mode, setMode] = React.useState(ToolMode.None);
+    const onShowHideButtons = props.onShowHideButtons;
+    const onRotationFinished = props.onRotationFinished;
+
+    const handleAction = (action:ToolsMenuAction) => {
+
+        switch (action.operation) {
+            case ToolMode.Rotate:
+                setMode(ToolMode.Rotate);
+                break;
+            case ToolMode.Translate:
+                setMode(ToolMode.Translate);
+                break;
+            case ToolMode.DrawSurface:
+                setMode(ToolMode.DrawSurface);
+                break;
+            default:
+                setMode(ToolMode.None);
+        }
+
+        if (props.onAction) {
+            props.onAction(action);
+        }
+    };
+
+    const rotateFinished = useCallback((finished: boolean, radians:number) => {
+        if (onRotationFinished) {
+            onRotationFinished(finished, radians)
+        }
+        setMode(ToolMode.None);
+        onShowHideButtons(true);
+    } , [onRotationFinished, onShowHideButtons]);
+
+    const onTranslationFinished = props.onTranslationFinished;
+    const translateFinished = useCallback((finished: boolean, xPos:number, yPos:number) => {
+        if (onTranslationFinished) {
+            onTranslationFinished(finished, xPos, yPos)
+        }
+        setMode(ToolMode.None);
+        onShowHideButtons(true);
+    } , [onTranslationFinished, onShowHideButtons]);
+
+    return (
+        <div className={"tools-menu"}>
+            <SimpleToolsMenu {...props} onAction={handleAction} />
+
+            {props.onRotationChanged && (
+                <RotateTool visible={mode === ToolMode.Rotate}
+                            onRotationFinished={rotateFinished}
+                            onRotationChanged={props.onRotationChanged}
+                            rotation={props.initialRotation ? props.initialRotation : 0} />
+            )}
+
+            {props.onTranslationChanged && (
+                <TranslateTool visible={mode === ToolMode.Translate}
+                               onTranslationFinished={translateFinished}
+                               onTranslationChanged={props.onTranslationChanged}
+                               xPos={props.initialXPos !== undefined ? props.initialXPos : 0}
+                               yPos={props.initialYPos !== undefined ? props.initialYPos : 0}
+                               min={props.minTranslation} max={props.maxTranslation} />
+            )}
+
+        </div>
     )
 }
