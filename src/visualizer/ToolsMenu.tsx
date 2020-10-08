eaf838c4903819c9c445de857b460aa5cb1cb7a1
Joel Teply <joel@cambriantech.com>
2020-10-07 19:30:18 -0500

    controls

--- src/visualizer/ToolsMenu.tsx
+++ src/visualizer/ToolsMenu.tsx
@@ -8,7 +8,8 @@ import MaterialIcon from "@material/react-material-icon";
 import {SpeedDialIcon} from "@material-ui/lab";
 import {RotateTool} from "./RotateTool";
 import {TranslateTool} from "./TranslateTool";
-import {useCallback} from "react";
+import {useCallback, useMemo} from "react";
+import {CBARSurface, CBARSurfaceAsset} from "react-home-ar";
 
 export enum ToolOperation {
     None='',
@@ -27,6 +28,8 @@ export type SimpleToolsMenuProperties = {
     name?:string
     hidden?:boolean
     direction?:'up' | 'down' | 'left' | 'right'
+    selectedAsset?:CBARSurfaceAsset|undefined
+    selectedSurface?:CBARSurface|undefined
     actions?:ToolsMenuAction[]
     icon?:React.ReactNode,
     openIcon?:React.ReactNode
@@ -38,16 +41,18 @@ export type ToolsMenuAction = {
     longName?:string|undefined
     icon:React.ReactNode
     operation?:string
+    requiresAsset?:boolean
+    requiresSurface?:boolean
 }
 
-export const ToolsMenuDefaultActions:ToolsMenuAction[] = [
+export const DefaultToolsMenuActions:ToolsMenuAction[] = [
     { icon: <MaterialIcon icon='add_a_photo' />, name: 'Photo', longName:'Take Photo', operation:ToolOperation.ChoosePhoto },
     { icon: <MaterialIcon icon='insert_photo' />, name: 'Scene', longName:'Change Scene', operation:ToolOperation.ChooseScene },
     { icon: <MaterialIcon icon='share' />, name: 'Share', longName:'Share Project', operation:ToolOperation.Share },
-    { icon: <MaterialIcon icon='rotate_right' />, name: 'Rotate', operation:ToolOperation.Rotate },
-    { icon: <MaterialIcon icon='open_with' />, name: 'Move', operation:ToolOperation.Translate },
-    { icon: <MaterialIcon icon='view_compact' />, name: 'Pattern', operation:ToolOperation.ChoosePattern },
-    { icon: <MaterialIcon icon='edit' />, name: 'Edit Surface', operation:ToolOperation.DrawSurface },
+    { icon: <MaterialIcon icon='rotate_right' />, name: 'Rotate', operation:ToolOperation.Rotate, requiresAsset:true },
+    { icon: <MaterialIcon icon='open_with' />, name: 'Move', operation:ToolOperation.Translate, requiresAsset:true },
+    { icon: <MaterialIcon icon='view_compact' />, name: 'Pattern', operation:ToolOperation.ChoosePattern, requiresAsset:true },
+    { icon: <MaterialIcon icon='edit' />, name: 'Edit Surface', operation:ToolOperation.DrawSurface, requiresSurface:true },
 ];
 
 export type ToolsMenuProperties = SimpleToolsMenuProperties & {
@@ -82,17 +87,27 @@ const useStyles = makeStyles((theme) => ({
 export function SimpleToolsMenu(props: SimpleToolsMenuProperties) {
     const classes = useStyles();
     const [open, setOpen] = React.useState(false);
-    const actions = props.actions ? props.actions : ToolsMenuDefaultActions;
     const isMobile = window.outerWidth < 400;
 
-    const handleClick = () => {
+    const handleClick = useCallback(() => {
         setOpen(!open);
-    };
+    }, [open]);
 
-    const handleAction = (action:ToolsMenuAction) => {
+    const handleAction = useCallback((action:ToolsMenuAction) => {
         setOpen(false);
         props.onAction(action);
-    };
+    }, [props.onAction]);
+
+    const actions = useMemo(()=>{
+        let actions:ToolsMenuAction[] = props.actions ? props.actions : DefaultToolsMenuActions;
+        if (!props.selectedAsset) {
+            actions = actions.filter(action=>!action.requiresAsset);
+        }
+        if (!props.selectedSurface) {
+            actions = actions.filter(action=>!action.requiresSurface);
+        }
+        return actions;
+    }, [props.actions, props.selectedAsset]);
 
     return (
         <SpeedDial
