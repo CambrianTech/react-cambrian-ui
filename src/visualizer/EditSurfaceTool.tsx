dbc50a5256668c1187465d7cb161b42d55dabd77
Joel Teply <joel@cambriantech.com>
2020-11-09 21:43:55 -0600

    undo method

--- src/visualizer/EditSurfaceTool.tsx
+++ src/visualizer/EditSurfaceTool.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import {useCallback} from "react";
 import {Fab} from "@material/react-fab";
 import '@material/react-fab/dist/fab.css';
 
@@ -6,39 +7,77 @@ import classes from "./EditSurfaceTool.scss";
 import MaterialIcon from "@material/react-material-icon";
 
 import {appendClassName} from "../internal/Utils"
-import {useCallback} from "react";
+import {CBARScene, CBARToolMode} from "react-home-ar";
 
 type EditSurfaceToolProps = {
-    visible: boolean
     className?:string
-    undoIcon?:React.ReactElement<HTMLElement>
-    redoIcon?:React.ReactElement<HTMLElement>
-    cancelIcon?:React.ReactElement<HTMLElement>
-    confirmIcon?:React.ReactElement<HTMLElement>
+    toolMode:CBARToolMode
+    scene:CBARScene|undefined
+    onToolChanged: (mode: CBARToolMode) => void
     onEditFinished: (commit: boolean) => void
 }
 
+export const eraserIcon = (<svg style={{width:"24px", height:"24px"}} viewBox="0 0 24 24">
+    <path fill="currentColor" d="M16.24,3.56L21.19,8.5C21.97,9.29 21.97,10.55 21.19,11.34L12,20.53C10.44,22.09 7.91,22.09 6.34,20.53L2.81,17C2.03,16.21 2.03,14.95 2.81,14.16L13.41,3.56C14.2,2.78 15.46,2.78 16.24,3.56M4.22,15.58L7.76,19.11C8.54,19.9 9.8,19.9 10.59,19.11L14.12,15.58L9.17,10.63L4.22,15.58Z" />
+</svg>);
+
+type ActionButtonProps = {
+    label:string
+    children:React.ReactElement<HTMLElement>
+    selected:boolean
+}
+
+export const ActionButton = React.memo<ActionButtonProps>(
+    (props) => {
+        let buttonClassName = appendClassName("edit-surface-tool-action-icon", classes.editSurfaceToolActionIcon);
+        if (props.selected) {
+            buttonClassName = appendClassName(buttonClassName, "selected");
+            buttonClassName = appendClassName(buttonClassName, classes.editSurfaceToolActionIconSelected);
+        }
+        return (
+            <div className={appendClassName("edit-surface-tool-action", classes.editSurfaceToolAction)}>
+                <div className={buttonClassName}>
+                    {props.children}
+                </div>
+                <div className={appendClassName("edit-surface-tool-action-label", classes.editSurfaceToolActionLabel)}>
+                    {props.label}
+                </div>
+            </div>
+        );
+    }
+);
+
 type EditSurfaceToolCachedProps = EditSurfaceToolProps & {
     onUndo: () => void
-    onRedo: () => void
 }
 
 export const EditSurfaceToolCached = React.memo<EditSurfaceToolCachedProps>(
-    (cProps) => {
-        if (cProps.visible) {
+    (props) => {
+        if (props.scene && (props.toolMode===CBARToolMode.DrawSurface || props.toolMode===CBARToolMode.EraseSurface)) {
             let className = appendClassName("edit-surface-tool", classes.editSurfaceTool);
-            className = appendClassName(className, cProps.className);
+            className = appendClassName(className, props.className);
 
             return (
                 <div className={className}>
                     <div className={appendClassName("edit-surface-tool-content", classes.editSurfaceToolContent)}>
-                        <div className={appendClassName("edit-surface-tool-slider", classes.editSurfaceToolButtons)}>
-                            <Fab className={appendClassName("undo", classes.editSurfaceToolSliderUndo)} icon={cProps.undoIcon ? cProps.undoIcon : <MaterialIcon icon='close'  />} onClick={() => cProps.onUndo()}  />
-                            <Fab className={appendClassName("redo", classes.editSurfaceToolSliderRedo)} icon={cProps.redoIcon ? cProps.redoIcon : <MaterialIcon icon='check' />} onClick={() => cProps.onRedo()} />
+                        <div className={appendClassName("edit-surface-tool-actions", classes.editSurfaceToolActions)}>
+                            <ActionButton selected={props.toolMode===CBARToolMode.DrawSurface} label={"Fill In"}>
+                                <Fab className={appendClassName("edit-surface-tool-draw", classes.editSurfaceToolDraw)} icon={<MaterialIcon icon='brush'  />} onClick={() => props.onToolChanged(CBARToolMode.DrawSurface)}  />
+                            </ActionButton>
+                            <ActionButton selected={props.toolMode===CBARToolMode.EraseSurface} label={"Erase"}>
+                                <Fab className={appendClassName("edit-surface-tool-erase", classes.editSurfaceToolErase)} icon={eraserIcon} onClick={() => props.onToolChanged(CBARToolMode.EraseSurface)} />
+                            </ActionButton>
                         </div>
-                        <div className={appendClassName("edit-surface-tool-footer", classes.editSurfaceToolFooter)}>
-                            <Fab className={appendClassName("cancel", classes.editSurfaceToolSliderCancel)} icon={cProps.cancelIcon ? cProps.cancelIcon : <MaterialIcon icon='close'  />} onClick={() => cProps.onEditFinished(false)}  />
-                            <Fab className={appendClassName("confirm", classes.editSurfaceToolSliderConfirm)} icon={cProps.confirmIcon ? cProps.confirmIcon : <MaterialIcon icon='check' />} onClick={() => cProps.onEditFinished(true)} />
+                        <div className={appendClassName("edit-surface-tool-primary-buttons", classes.editSurfaceToolPrimaryButtons)}>
+                            <div className={appendClassName("edit-surface-tool-button", classes.editSurfaceToolButton)}>
+                                <Fab disabled={props.scene.historyLength === 0} className={appendClassName("edit-surface-tool-undo", classes.editSurfaceToolUndo)} icon={<MaterialIcon icon='undo'  />} onClick={() => props.onUndo()}  />
+                            </div>
+                            <div className={appendClassName("edit-surface-tool-button", classes.editSurfaceToolButton)}>
+                                <Fab className={appendClassName("edit-surface-tool-cancel", classes.editSurfaceToolCancel)} icon={<MaterialIcon icon='close'  />} onClick={() => props.onEditFinished(false)}  />
+                            </div>
+                            <div className={appendClassName("edit-surface-tool-button", classes.editSurfaceToolButton)}>
+                                <Fab className={appendClassName("edit-surface-tool-confirm", classes.editSurfaceToolConfirm)} icon={<MaterialIcon icon='check' />} onClick={() => props.onEditFinished(true)} />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -47,21 +86,19 @@ export const EditSurfaceToolCached = React.memo<EditSurfaceToolCachedProps>(
         return null;
     },
     (prevProps, nextProps) => {
-        return prevProps.visible === nextProps.visible;
+        return prevProps.toolMode === nextProps.toolMode;
     }
 );
 
 export function EditSurfaceTool(props: EditSurfaceToolProps) {
 
     const onUndo = useCallback(() => {
-        console.log("undo")
-    }, []);
-
-    const onRedo = useCallback(() => {
-        console.log("redo")
+        if (props.scene) {
+            props.scene.undoLast();
+        }
     }, []);
 
     return (
-        <EditSurfaceToolCached {...props} onUndo={onUndo} onRedo={onRedo} />
+        <EditSurfaceToolCached {...props} onUndo={onUndo} />
     )
 }
