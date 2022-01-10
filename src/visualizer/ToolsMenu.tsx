cc48f035187c5eec109d871cbd6e5ebd69fb60f9
Joel Teply <joel@cambriantech.com>
2022-01-10 01:10:44 -0600

    yikes

--- src/visualizer/ToolsMenu.tsx
+++ src/visualizer/ToolsMenu.tsx
@@ -19,13 +19,17 @@ export type ToolsMenuAction = {
     requiresSurface?:boolean
 }
 
-export const DefaultToolsMenuActions:ToolsMenuAction[] = [
+export const DefaultAssetMenuActions:ToolsMenuAction[] = [
     { icon: <Icon>clear</Icon>, name: 'Remove', longName:'Remove', operation:ToolOperation.Remove, requiresAsset:true },
-    { icon: <Icon>add_a_photo</Icon>, name: 'Photo', operation:ToolOperation.ChoosePhoto },
-    { icon: <Icon>insert_photo</Icon>, name: 'Scene', longName:'Change Scene', operation:ToolOperation.ChooseScene },
-    { icon: <Icon>share</Icon>, name: 'Share', longName:'Share Project', operation:ToolOperation.Share },
     { icon: <Icon>rotate_right</Icon>, name: 'Rotate', operation:CBARToolMode.Rotate, requiresAsset:true },
     { icon: <Icon>open_with</Icon>, name: 'Move', operation:CBARToolMode.Translate, requiresAsset:true },
     { icon: <Icon>view_compact</Icon>, name: 'Pattern', operation:ToolOperation.ChoosePattern, requiresAsset:true },
     { icon: <Icon>edit</Icon>, name: 'Edit Surface', operation:CBARToolMode.DrawSurface, requiresSurface:true },
+];
+
+export const DefaultToolsMenuActions:ToolsMenuAction[] = [
+    { icon: <Icon>add_a_photo</Icon>, name: 'Photo', operation:ToolOperation.ChoosePhoto },
+    { icon: <Icon>insert_photo</Icon>, name: 'Scene', longName:'Change Scene', operation:ToolOperation.ChooseScene },
+    { icon: <Icon>share</Icon>, name: 'Share', longName:'Share Project', operation:ToolOperation.Share },
+    ...DefaultAssetMenuActions
 ];
